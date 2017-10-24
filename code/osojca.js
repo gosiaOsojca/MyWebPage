94f9c1df950b017fc361ec8b93bd0d2f1d63ad0c
Unknown <gosia.osojca@gmail.com>
2017-10-24 21:45:34 +0200

    removed CC field

--- code/osojca.js
+++ code/osojca.js
@@ -1,6 +1,6 @@
 function sendMail() {
     var link = "mailto:gosia.osojca@gmail.com"
-             + "?cc=wojciech.pulawski@gmail.com"
+             + "?cc="
              + "&subject=" + escape(document.getElementById('name').value) + " - " +  escape(document.getElementById('email').value)
              + "&body=" + escape(document.getElementById('message').value)
     ;
